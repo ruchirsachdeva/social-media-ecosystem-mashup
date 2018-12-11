0eeea0a5b14f331a3fd0b443d487784a53c3432e
Ruchir Sachdeva <ruchirsachdeva@yahoo.com>
2018-12-11 07:35:07 +0530

    fixed build output path in angular.json to dist

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-mashup'`, () => {
+  it(`should have as title 'social-medial-ecosystem-mashup'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-mashup');
+    expect(app.title).toEqual('social-medial-ecosystem-mashup');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-mashup!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to social-medial-ecosystem-mashup!');
   });
 });
